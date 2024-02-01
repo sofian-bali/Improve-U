b6b8f0d385a79d643869db8b83cc3975c213519a
sofian-bali <95469123+sofian-bali@users.noreply.github.com>
2024-02-01 15:00:20 +0100

    Update server.js and package.json

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
-import express from "express";
-const port = 5000;
+import express from 'express';
 
+const port = 3000;
 const app = express();
 
+app.get("/home", (req,res)=>{
+    
+    res.send("Server is ready");
 
-
+});
 
 app.listen(port, () => {
     console.log(`Serve at http://localhost:${port}`);
