49b420c51060e7921c11ed17e0725de54c1ff911
Maxime Lemesle <maxime.lemesle7@gmail.com>
2024-03-17 22:47:45 +0100

    🚀 Install Prisma and connect the database | 17/03

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,30 @@
 import express from 'express';
 
-const port = 3000;
+// Création du serveur
 const app = express();
+const port = 3000;
 
-app.get("/home", (req,res)=>{
-    
-    res.send("Server is ready");
-    
-
+// Affichage de la page Home
+app.get("/", (req,res)=>{
+    res.status(200).json({"message": "Server is ready"});
 });
 
-app.listen(port, () => {
-    console.log(`Serve at http://localhost:${port}`);
+// Routes sport
+import sport from './routes/sport.routes.js';
+app.use('/sport', sport);
 
-    }  
-);
+
+// Routes nutrition
+import nutrition from './routes/nutrition.routes.js';
+app.use('/nutrition', nutrition);
+
+
+// Routes sante
+import sante from './routes/sante.routes.js';
+app.use('/sante', sante);
+
+
+// Démarage du serveur
+app.listen(port, () => {
+    console.log(`Le serveur est en ligne à l'url: http://localhost:${port} !`);
+});
