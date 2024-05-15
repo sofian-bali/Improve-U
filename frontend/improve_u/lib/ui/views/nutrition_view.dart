f2a7a6d9cbe240973343d3f2f4590e6fa752f3a3
Maxime Lemesle <maxime.lemesle7@gmail.com>
2024-05-15 09:27:35 +0200

    ✨ Start creating navigation | 15/05

--- frontend/improve_u/lib/ui/views/nutrition_view.dart
+++ frontend/improve_u/lib/ui/views/nutrition_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:improve_u/ui/widgets/custom_navigation.dart';
 import 'package:improve_u/ui/widgets/nutrition/nutrition_progress_card.dart';
 
 class NutritionView extends StatelessWidget {
@@ -6,33 +7,38 @@ class NutritionView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: Column(
-        children: <Widget>[
-          /// Titre
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+    return Column(
+      children: [
+        /// Navigation
+        const CustomNavigation(),
+
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
             children: <Widget>[
-              Text(
-                'Nutrition',
-                style: Theme.of(context)
-                    .textTheme
-                    .displayLarge
-                    ?.copyWith(color: Theme.of(context).colorScheme.onPrimary),
+              /// Titre
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Nutrition',
+                    style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                        color: Theme.of(context).colorScheme.onPrimary),
+                  ),
+                ],
               ),
+
+              // Espacement
+              const SizedBox(
+                height: 32,
+              ),
+
+              /// Affichage de la progression
+              const NutritionProgressCard(),
             ],
           ),
-
-          // Espacement
-          const SizedBox(
-            height: 32,
-          ),
-
-          /// Affichage de la progression
-          const NutritionProgressCard(),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
