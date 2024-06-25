b0fc51f3aeb37002aa34e12091b2b3269777622e
sofian-bali <95469123+sofian-bali@users.noreply.github.com>
2024-06-25 11:45:28 +0200

    ✨ Add video splashscreen

--- frontend/improve_u/lib/ui/screens/onboarding/splash_screen.dart
+++ frontend/improve_u/lib/ui/screens/onboarding/splash_screen.dart
@@ -1,6 +1,6 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:improve_u/ui/screens/onboarding/connexion_screen.dart';
+import 'package:video_player/video_player.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,25 +9,44 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
+class _SplashScreenState extends State<SplashScreen> {
+  late VideoPlayerController _controller;
+
   @override
   void initState() {
     super.initState();
+    _controller = VideoPlayerController.asset('assets/logo/Animation_ImproveU.mp4')
+      ..initialize().then((_) {
+        setState(() {});
+        _controller.play();
+      });
 
-    Future.delayed(const Duration(seconds: 1), () {
-      Navigator.of(context).pushReplacement(
-        PageRouteBuilder(
-          transitionDuration: const Duration(milliseconds: 700),
-          pageBuilder: (context, animation, secondaryAnimation) {
-            return FadeTransition(
-              opacity: animation,
-              child: const ConnexionScreen(),
-            );
-          },
-        ),
-      );
+    _controller.setLooping(false);
+    _controller.addListener(() {
+      if (!_controller.value.isPlaying && _controller.value.position == _controller.value.duration) {
+        _navigateToConnexionScreen();
+      }
     });
+  }
+
+  void _navigateToConnexionScreen() {
+    Navigator.of(context).pushReplacement(
+      PageRouteBuilder(
+        transitionDuration: const Duration(milliseconds: 700),
+        pageBuilder: (context, animation, secondaryAnimation) {
+          return FadeTransition(
+            opacity: animation,
+            child: const ConnexionScreen(),
+          );
+        },
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -35,11 +54,22 @@ class _SplashScreenState extends State<SplashScreen>
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.onBackground,
       body: Center(
-        child: Image.asset(
-          'assets/logo/logo_vertical_white.png',
-          height: 200,
-        ),
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              )
+            : Image.asset(
+                'assets/logo/logo_vertical_white.png',
+                height: 200,
+              ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: const SplashScreen(),
+  ));
 }
